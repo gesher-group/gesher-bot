9ff8cc6ac982b1360e38ca9db0146469e2e00651
Alex Price <alex@alexprice.co>
2017-05-07 21:16:04 -0700

    Implemented the botkit starter package, going to hack on this for a bit

--- handlers/in.js
+++ handlers/in.js
@@ -1,19 +1,4 @@
 var request = require('request')
-var env = require('../tokens.js')
-var slack = require('slack')
-var HOOK_URL = 'https://hooks.slack.com/services/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'
-
-
-let bot = slack.rtm.client()
-let token = env.SLACK_BOT_TOKEN
-
-bot.hello(message => {
-  console.log(message)
-  // console.log(`Got a message: ${message}`)
-  bot.close()
-})
-
-bot.listen({token})
 
 module.exports = (req, res, next) => {
   console.log('in.js / req ', req)
