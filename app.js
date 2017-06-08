c845fce2a65ab4f2958e63ec1532690f335ad8d6
Daniel Boles <Daniel Boles>
2017-06-07 18:07:41 -0700

    invite line

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ controller.hears(['ping'], 'direct_mention', (bot, message) => {
   })
 })
 
+controller.hears(['invite'], 'direct_mention', (bot, message) => {
+  //
+})
+
 controller.hears(['help', 'roadmap', 'what do you do'], ['direct_message', 'mention'], (bot, message) => {
   bot.api.reactions.add({
     timestamp: message.ts,
